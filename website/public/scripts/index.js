43b9ec712e76627fa705f1153d986a3410ec66d3
Max Knyazev <magmel@Maxs-MBP.Dlink>
2018-09-16 10:40:13 +0300

    manifesto

--- website/public/scripts/index.js
+++ website/public/scripts/index.js
@@ -3,15 +3,23 @@ const ready = () => {
 
 	const facesEl = document.getElementsByClassName('menu__faces')[0];
 	const manifestEl = document.getElementsByClassName('menu__manifest')[0];
+	const pageFacesEl = document.getElementsByClassName('page_faces')[0];
+	const pageManifestEl = document.getElementsByClassName('page_manifest')[0];
 
 	facesEl.addEventListener('click', () => {
 		facesEl.classList.toggle('menu__active');
 		manifestEl.classList.toggle('menu__active');
+
+		pageFacesEl.classList.toggle('hidden');
+		pageManifestEl.classList.toggle('hidden');
 	});
 
 	manifestEl.addEventListener('click', () => {
 		facesEl.classList.toggle('menu__active');
 		manifestEl.classList.toggle('menu__active');
+
+		pageFacesEl.classList.toggle('hidden');
+		pageManifestEl.classList.toggle('hidden');
 	});
 };
 
