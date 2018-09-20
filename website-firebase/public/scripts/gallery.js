169b38232e9994706bf0ee6b6bd52fabe07fdb5f
Max Knyazev <magmel@Maxs-MacBook-Pro.local>
2018-09-20 23:45:48 +0300

    tests

--- website-firebase/public/scripts/gallery.js
+++ website-firebase/public/scripts/gallery.js
@@ -1,5 +1,8 @@
 const galleryReady = () => {
 	console.log('gallery is ready.');
+
+	const picturesEl = document.getElementsByClassName('pictures').item(0);
+	// TODO download, share, etc.
 };
 
 document.addEventListener('DOMContentLoaded', galleryReady);
